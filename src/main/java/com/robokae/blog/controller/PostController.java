7e45f8ae9e417bc505cbc1f1769ef005d8de7401
Alexander Hom <alexanderhom19@gmail.com>
2024-12-28 12:36:38 -0800

    Update PostController to use builder for generating responses

--- src/main/java/com/robokae/blog/controller/PostController.java
+++ src/main/java/com/robokae/blog/controller/PostController.java
@@ -3,7 +3,6 @@ package com.robokae.blog.controller;
 import com.robokae.blog.service.PostService;
 import com.robokae.blog.model.Post;
 import com.robokae.common.model.Response;
-import com.robokae.common.util.ResponseUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,35 +20,50 @@ public class PostController {
     @GetMapping("/posts")
     public ResponseEntity<Response> getAllPosts() {
         List<Post> posts = postService.getAllPosts();
-        Response response = ResponseUtil.getResponse(HttpStatus.OK, "", posts);
-        return new ResponseEntity<>(response, response.getStatus());
+        Response response = Response.builder()
+                .status(HttpStatus.OK.value())
+                .data(posts)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/post/{title}")
     public ResponseEntity<Response> getPost(@PathVariable("title") String title) {
         Post post = postService.getPost(title);
-        Response response = ResponseUtil.getResponse(HttpStatus.OK, "", post);
-        return new ResponseEntity<>(response, response.getStatus());
+        Response response = Response.builder()
+                .status(HttpStatus.OK.value())
+                .data(post)
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/post")
     public ResponseEntity<Response> createPost(@RequestBody Post post) {
         postService.savePost(post);
-        Response response = ResponseUtil.getResponse(HttpStatus.OK, "Successfully created post", "");
-        return new ResponseEntity<>(response, response.getStatus());
+        Response response = Response.builder()
+                .status(HttpStatus.OK.value())
+                .message("Successfully created post")
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @PutMapping("/post/{title}")
     public ResponseEntity<Response> updatePost(@PathVariable("title") String title, @RequestBody Post post) {
         postService.updatePost(title, post);
-        Response response = ResponseUtil.getResponse(HttpStatus.CREATED, "Successfully updated post", "");
-        return new ResponseEntity<>(response, response.getStatus());
+        Response response = Response.builder()
+                .status(HttpStatus.CREATED.value())
+                .message("Successfully updated post")
+                .build();
+        return ResponseEntity.ok(response);
     }
 
     @DeleteMapping("/post/{title}")
     public ResponseEntity<Response> deletePost(@PathVariable("title") String title) {
         postService.deletePost(title);
-        Response response = ResponseUtil.getResponse(HttpStatus.OK, "Successfully deleted post", "");
-        return new ResponseEntity<>(response, response.getStatus());
+        Response response = Response.builder()
+                .status(HttpStatus.OK.value())
+                .message("Successfully deleted post")
+                .build();
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/com/robokae/common/model/Response.java
+++ src/main/java/com/robokae/common/model/Response.java
@@ -1,16 +1,17 @@
 package com.robokae.common.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.http.HttpStatus;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Response {
 
-    private HttpStatus status;
+    private int status;
     private String message;
     private Object data;
 }
