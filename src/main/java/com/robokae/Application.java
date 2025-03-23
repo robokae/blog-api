92bdd2297eef1b5de144451c866887e5f50abd0d
Alexander Hom <alexanderhom19@gmail.com>
2025-03-22 18:18:44 -0700

    Clean up Spring Security config

--- src/main/java/com/robokae/Application.java
+++ src/main/java/com/robokae/Application.java
@@ -2,10 +2,8 @@ package com.robokae;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.profiles.active:local}.yaml")
 public class Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/robokae/blog/config/SecurityConfig.java
+++ src/main/java/com/robokae/blog/config/SecurityConfig.java
@@ -1,54 +1,31 @@
 package com.robokae.blog.config;
 
-import com.robokae.blog.security.filter.JwtTokenFilter;
-import com.robokae.blog.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private JwtTokenFilter jwtTokenFilter;
+    @Value("${security.enable-csrf}")
+    private boolean enableCsrf;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
 
-        http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
-                authorizationManagerRequestMatcherRegistry
-                        .requestMatchers("/admin/**").authenticated()
-                        .requestMatchers("/api/**").permitAll());
+        if (!enableCsrf) {
+            http.csrf(AbstractHttpConfigurer::disable);
+        }
+
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/admin/**").authenticated()
+                .requestMatchers("/api/**").permitAll());
 
         return http.build();
-    }
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Bean
-    public UserDetailsService userDetailsService() {
-        return username -> userRepository.findByUsername(username);
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
-        throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
     }
 }
