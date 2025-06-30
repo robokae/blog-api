2357e44940864dd1194cb4229cdb1e8961a92e5f
Alexander Hom <alexanderhom19@gmail.com>
2025-06-29 19:52:43 -0700

    Fix tests

--- src/main/java/com/robokae/Application.java
+++ src/main/java/com/robokae/Application.java
@@ -3,7 +3,7 @@ package com.robokae;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration.class})
 public class Application {
 
 	public static void main(String[] args) {
